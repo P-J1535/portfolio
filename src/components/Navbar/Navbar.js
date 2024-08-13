fee61a1efe400e1f3e5d631a6634032f99ef074f
prathamesh <prathameshjepal97@gmail.com>
2024-08-14 00:05:06 +0530

    Ui changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import styles from './Navbar.module.css';
+import logo from '../../assets/images/logo.png'
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,22 +11,37 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleLinkClick = (index) => {
-    setActiveIndex(index);
-    setIsOpen(false); 
+  const handleLinkClick = (index, item) => {
+    if (item === 'Resume') {
+      handleDownload();
+    } else {
+      setActiveIndex(index);
+    }
+    setIsOpen(false);
+  };
+
+  const handleDownload = () => {
+    const resumeUrl = "/Resume/PrathameshJepal.pdf";
+    window.open(resumeUrl, '_blank');
+  };
+
+  const handleHomePage = () => {
+    window.location.href = '/';
   };
 
   return (
     <nav className={styles.navbar}>
-      <div className={styles.navbarLogo}>{`< PJ >`}</div>
+      <div className={styles.navbarLogo}>
+      
+      <img src={logo} alt="logo" onClick={handleHomePage} />
+      </div>
       <ul className={`${styles.navbarMenu} ${isOpen ? styles.active : ''}`}>
-        {['Home', 'Projects', 'Resume','Contact'].map((item, index) => (
+        {['Home', 'Projects', 'About', 'Resume', 'Contact'].map((item, index) => (
           <li key={index}>
             <a
-              href={`#${item.toLowerCase()}`}
-              
+              href={item !== 'Resume' ? `#${item.toLowerCase()}` : '#'}
               className={activeIndex === index ? styles.active : ''}
-              onClick={() => handleLinkClick(index)}
+              onClick={() => handleLinkClick(index, item)}
             >
               {item}
             </a>
